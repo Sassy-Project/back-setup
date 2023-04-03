42911289106ff26c08aff919a31f915fb3d9079c
youhobin <ghdb132@naver.com>
2023-04-03 12:41:06 +0900

    feat : init commit
    init commit

---
+++ src/test/java/com/projectsassy/sassy/SassyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsassy.sassy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SassyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
