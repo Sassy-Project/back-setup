449f9a8cdff6f6658c40459f629674f34e305c5f
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-04-05 23:29:38 +0900

    feat : email duplicateservice

--- src/main/java/com/projectsassy/sassy/user/controller/UserController.java
+++ src/main/java/com/projectsassy/sassy/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.projectsassy.sassy.user.controller;
 
+import com.projectsassy.sassy.user.dto.DuplicateEmailDto;
 import com.projectsassy.sassy.user.dto.DuplicateLoginIdDto;
 import com.projectsassy.sassy.user.dto.UserJoinDto;
 import com.projectsassy.sassy.user.service.UserService;
@@ -29,8 +30,18 @@ public class UserController {
     @GetMapping("/duplicateLoginId")
     public ResponseEntity duplicateLoginId(@RequestBody DuplicateLoginIdDto duplicateLoginIdDto) {
         userService.duplicateLoginId(duplicateLoginIdDto);
+        // false 값이 넘어오기 때문에 api 테스트 필요
+        return ResponseEntity.ok().body("사용 가능한 아이디입니다.");
+    }
 
-        return ResponseEntity.ok().body("가입 가능한 아이디입니다.");
+    /**
+     * 이메일 중복검사
+     */
+    @GetMapping("/duplicateEmail")
+    public ResponseEntity duplicateEmail(@RequestBody DuplicateEmailDto duplicateEmailDto) {
+        userService.duplicateEmail(duplicateEmailDto);
+
+        return ResponseEntity.ok().body("사용 가능한 이메일 입니다.");
     }
 
 }
