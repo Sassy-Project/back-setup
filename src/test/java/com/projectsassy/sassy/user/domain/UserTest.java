275fb23a07003c68dc13ca089e6dccd3d0463992
youhobin <ghdb132@naver.com>
2023-04-07 15:01:41 +0900

    test : validate email
    validate email test

--- src/test/java/com/projectsassy/sassy/user/domain/UserTest.java
+++ src/test/java/com/projectsassy/sassy/user/domain/UserTest.java
@@ -1,11 +1,13 @@
 package com.projectsassy.sassy.user.domain;
 
+import com.projectsassy.sassy.exception.CustomIllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
 
@@ -26,6 +28,15 @@ public class UserTest {
 
         //then
         assertThat("1q2w3ee").isNotEqualTo(user.getPassword());
+    }
+
+    @Test
+    @DisplayName("이메일 유효성 검사")
+    public void validateEmail() throws Exception {
+        assertThatThrownBy(() -> {
+            User.of("qwer1234", "1q2w3e", "haha", "email", "man", "enfp", "image");
+        })
+            .isInstanceOf(CustomIllegalArgumentException.class);
     }
 
 }
