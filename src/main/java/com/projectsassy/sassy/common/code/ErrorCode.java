5cc502bfb243f5002431747a90e6a9f36292862a
youhobin <ghdb132@naver.com>
2023-04-10 16:45:43 +0900

    feat : unauthorized exception
    feat : unauthorized exception

--- src/main/java/com/projectsassy/sassy/common/code/ErrorCode.java
+++ src/main/java/com/projectsassy/sassy/common/code/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     // Common
     INTERNAL_SERVER_ERROR(500, "C001", "서버 내부 오류입니다."),
     INVALID_INPUT_VALUE(400, "C002", "잘못된 입력입니다."),
+    UNAUTHORIZED(401, "C003", "인증되지 않은 사용자입니다."),
 
     // User
     DUPLICATE_EMAIL(400, "U001", "중복된 이메일입니다."),
