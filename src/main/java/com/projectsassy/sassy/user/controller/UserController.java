bab4fa0346589fbbc0afa6efe9c6b69f539c5a8c
youhobin <ghdb132@naver.com>
2023-04-16 22:38:54 +0900

    Update UserController.java

--- src/main/java/com/projectsassy/sassy/user/controller/UserController.java
+++ src/main/java/com/projectsassy/sassy/user/controller/UserController.java
@@ -90,6 +90,7 @@ public class UserController {
             .path("/")
             .sameSite("None")
             .secure(true)
+            .domain("http://test.projectsassy.net/")
             .build();
         response.addCookie(new Cookie("cookie", "cookieasdf"));
         response.setHeader("Set-Cookie", cookie.toString());
