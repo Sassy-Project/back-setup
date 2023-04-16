ae7d36bd4d627c332e690e4a92d7640f9ac729bb
youhobin <ghdb132@naver.com>
2023-04-16 18:34:53 +0900

    fix : cookie
    fix cookie

--- src/main/java/com/projectsassy/sassy/user/controller/UserController.java
+++ src/main/java/com/projectsassy/sassy/user/controller/UserController.java
@@ -89,6 +89,7 @@ public class UserController {
         ResponseCookie cookie = ResponseCookie.from("JSESSIONID", session.getId())
             .path("/")
             .sameSite("None")
+            .domain("http://test.projectsassy.net/")
             .build();
         response.addCookie(new Cookie("JSESSIONID", session.getId()));
         response.setHeader("Set-Cookie", cookie.toString());
