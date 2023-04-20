cc540217212e6ea9057a7123b6d0c265011fe711
youhobin <ghdb132@naver.com>
2023-04-20 22:14:29 +0900

    fix : fix query error
     fix query error

--- src/main/java/com/projectsassy/sassy/chatting/repository/ChattingRoomRepository.java
+++ src/main/java/com/projectsassy/sassy/chatting/repository/ChattingRoomRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
 
-    @Query("select c from ChattingRoom c " +
-        "join fetch c.sendUser su " +
-        "left join c.receiveUser ru " +
-        "where su.mbti = :mbti and ru is null and c.wantMbti = :myMbti")
-    List<ChattingRoom> findRoomBySelectMbti(@Param("mbti") String mbti, @Param("myMbti") String myMbti);
+//    @Query("select c from ChattingRoom c " +
+//        "join fetch c.sendUser su " +
+//        "left join c.receiveUser ru " +
+//        "where su.mbti = :mbti and ru is null and c.wantMbti = :myMbti")
+//    List<ChattingRoom> findRoomBySelectMbti(@Param("mbti") String mbti, @Param("myMbti") String myMbti);
 
 }
