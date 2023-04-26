779eef24308a4b7d58660f5689a44305648e5708
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-04-26 17:41:25 +0900

    feat : post
    단건조회 개발중, post/** cors 추가

---
+++ src/main/java/com/projectsassy/sassy/post/dto/LookUpPostResponse.java
@@ -0,0 +1,12 @@
+package com.projectsassy.sassy.post.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class LookUpPostResponse {
+
+    private String nickname;
+    private String title;
+    private String content;
+    private String category;
+}
--- src/main/java/com/projectsassy/sassy/post/service/PostService.java
+++ src/main/java/com/projectsassy/sassy/post/service/PostService.java
@@ -1,18 +1,23 @@
 package com.projectsassy.sassy.post.service;
 
+import com.projectsassy.sassy.common.code.ErrorCode;
+import com.projectsassy.sassy.common.exception.CustomIllegalStateException;
 import com.projectsassy.sassy.post.domain.Post;
 import com.projectsassy.sassy.post.dto.HomeResponse;
 import com.projectsassy.sassy.post.dto.CreatePostRequest;
+import com.projectsassy.sassy.post.dto.LookUpPostResponse;
 import com.projectsassy.sassy.post.repository.PostRepository;
 import com.projectsassy.sassy.user.domain.User;
 import com.projectsassy.sassy.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,10 +28,10 @@ public class PostService {
     private final UserService userService;
 
     //게시판 홈
-    public List<HomeResponse> getPostHome(Integer limit, Integer page) {
-        PageRequest pageRequest = PageRequest.of(page, limit);
+    public List<HomeResponse> getPostHome(Pageable pageable) {
 
-        Page<Post> postOfPage = postRepository.findAll(pageRequest);
+        Page<Post> postOfPage = postRepository.findAll(pageable);
+
 
         return postOfPage.stream().map(p -> new HomeResponse(
                         p.getId(), p.getTitle(), p.getUser().getNickname(), p.getCategory().toString(),
@@ -39,9 +44,18 @@ public class PostService {
         User user = userService.findById(createPostRequest.getUserId());
 
         Post post = Post.of(createPostRequest, user);
-
         Post savedPost = postRepository.save(post);
 
         return savedPost.getId();
+    }
+
+
+    public List<LookUpPostResponse> lookUpPost(Long postId) {
+        Optional<Post> findPost = postRepository.findById(postId);
+        Post.countPost();
+        return findPost.stream().map(
+                p -> new LookUpPostResponse(p.getTitle(), p.getContent(),
+                        p.getUser().getNickname(), p.getCategory().toString())
+        ).collect(Collectors.toList());
     }
 }
