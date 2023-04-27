d49a59b2769085288f85247e3a978f573c917770
youhobin <ghdb132@naver.com>
2023-04-27 22:19:23 +0900

    fix : add getter
    add getter

--- src/main/java/com/projectsassy/sassy/user/dto/IdResponse.java
+++ src/main/java/com/projectsassy/sassy/user/dto/IdResponse.java
@@ -1,8 +1,10 @@
 package com.projectsassy.sassy.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class IdResponse {
 
     private Long userId;
