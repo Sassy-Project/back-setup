5cdff181582aae4e2331492405df8531ce5e911d
youhobin <ghdb132@naver.com>
2023-05-03 13:09:07 +0900

    refactor : remove import
    remove no use import

--- src/main/java/com/projectsassy/sassy/token/CustomUserDetailsService.java
+++ src/main/java/com/projectsassy/sassy/token/CustomUserDetailsService.java
@@ -3,8 +3,6 @@ package com.projectsassy.sassy.token;
 import com.projectsassy.sassy.user.domain.User;
 import com.projectsassy.sassy.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
