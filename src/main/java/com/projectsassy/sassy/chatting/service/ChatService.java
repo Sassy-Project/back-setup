f6c24e0a7a664629a082cfbdf3c32beebf1da085
youhobin <ghdb132@naver.com>
2023-05-04 16:35:53 +0900

    refactor : ChatService
    refactor ChatService map

--- src/main/java/com/projectsassy/sassy/chatting/service/ChatService.java
+++ src/main/java/com/projectsassy/sassy/chatting/service/ChatService.java
@@ -1,10 +1,7 @@
 package com.projectsassy.sassy.chatting.service;
 
 import com.projectsassy.sassy.chatting.data.MatchingMbtiData;
-import com.projectsassy.sassy.chatting.dto.ChatCloseResponse;
-import com.projectsassy.sassy.chatting.dto.MatchResponse;
-import com.projectsassy.sassy.chatting.dto.RoomInformation;
-import com.projectsassy.sassy.chatting.dto.WaitingRequest;
+import com.projectsassy.sassy.chatting.dto.*;
 import com.projectsassy.sassy.user.domain.User;
 import com.projectsassy.sassy.user.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +10,17 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.projectsassy.sassy.chatting.data.ChattingData.*;
-import static com.projectsassy.sassy.chatting.data.ChattingData.waiting;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 @Service
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ChatService {
-
+    public ConcurrentHashMap<Long, String> waiting = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<String, MatchingMbtiData> waitingData = new ConcurrentHashMap<>();
+    public ConcurrentHashMap<String, Long> chattingRoomSessions = new ConcurrentHashMap<>();
     private final SimpMessageSendingOperations simpMessageSendingOperations;
     private final UserService userService;
     private final ChattingRoomService chattingRoomService;
@@ -43,6 +42,7 @@ public class ChatService {
         }
     }
 
+    @Transactional
     private void startMatching(User user, String myMbti, String selectMbti) {
         for (Long waitingUserId : waiting.keySet()) {
             Long userId = user.getId();
@@ -55,29 +55,51 @@ public class ChatService {
             String waitingUserSelectMbti = waitingMatchingMbtiData.getSelectMbti();
 
             if (selectMbti.equals(waitingUserMbti) && myMbti.equals(waitingUserSelectMbti)) {
-                RoomInformation roomInformation = chattingRoomService.createChattingRoom(user, waitingUserId);
-                Long roomId = roomInformation.getRoomId();
-
-                MatchResponse myMatchResponse = new MatchResponse("match", String.valueOf(roomId), roomInformation.getMatchedUserNickname());
-                MatchResponse matchedUserResponse = new MatchResponse("match", String.valueOf(roomId), user.getNickname());
-                simpMessageSendingOperations.convertAndSend("/sub/chat/wait/" + userId, myMatchResponse);
-                simpMessageSendingOperations.convertAndSend("/sub/chat/wait/" + waitingUserId, matchedUserResponse);
-
-                chattingRoomSessions.put(waiting.get(userId), roomId);
-                chattingRoomSessions.put(waiting.get(waitingUserId), roomId);
-
-                waitingData.remove(waiting.get(userId));
-                waitingData.remove(waiting.get(waitingUserId));
-                waiting.remove(userId);
-                waiting.remove(waitingUserId);
+                succeedMatching(user, waitingUserId, userId);
                 break;
             }
 
         }
     }
 
-    public void sendCloseMessage(Long roomId) {
-        ChatCloseResponse chatCloseResponse = new ChatCloseResponse(roomId);
-        simpMessageSendingOperations.convertAndSend("/sub/chat/match/" + roomId, chatCloseResponse);
+    private void succeedMatching(User user, Long waitingUserId, Long userId) {
+        RoomInformation roomInformation = chattingRoomService.createChattingRoom(user, waitingUserId);
+        Long roomId = roomInformation.getRoomId();
+
+        MatchResponse myMatchResponse = new MatchResponse("match", String.valueOf(roomId), roomInformation.getMatchedUserNickname());
+        MatchResponse matchedUserResponse = new MatchResponse("match", String.valueOf(roomId), user.getNickname());
+        simpMessageSendingOperations.convertAndSend("/sub/chat/wait/" + userId, myMatchResponse);
+        simpMessageSendingOperations.convertAndSend("/sub/chat/wait/" + waitingUserId, matchedUserResponse);
+
+        chattingRoomSessions.put(waiting.get(userId), roomId);
+        chattingRoomSessions.put(waiting.get(waitingUserId), roomId);
+
+        waitingData.remove(waiting.get(userId));
+        waitingData.remove(waiting.get(waitingUserId));
+        waiting.remove(userId);
+        waiting.remove(waitingUserId);
+        return;
+    }
+
+    public void exitWaiting(String sessionId) {
+        if (waitingData.get(sessionId) != null) {
+            waitingData.remove(sessionId);
+
+            for (Long userId : waiting.keySet()) {
+                if (waiting.get(userId).equals(sessionId)) {
+                    waiting.remove(userId);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void exitChattingRoom(String sessionId) {
+        if (chattingRoomSessions.get(sessionId) != null) {
+            Long roomId = chattingRoomSessions.get(sessionId);
+            ChatCloseResponse chatCloseResponse = new ChatCloseResponse(roomId);
+            simpMessageSendingOperations.convertAndSend("/sub/chat/match/" + roomId, chatCloseResponse);
+            chattingRoomSessions.remove(sessionId);
+        }
     }
 }
